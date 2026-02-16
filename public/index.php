ef88f2e236630f2baf7944a69b9a6ed60e261a8d
Irene <irene.ucciero91@gmail.com>
2026-02-16 14:09:20 +0100

    metodi

--- public/index.php
+++ public/index.php
@@ -8,14 +8,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new App();
 $request = Request::fromGlobals(); // metodo statico per raccogliere la richiesta del browser che immagazzino nella variabile $request
-print_r($request);
+//print_r($request);
 $response = $app->handle($request); // elaborazione risposta
 
 $response->send(); // invio la risposta al browser
 
-echo "Get: \n";
-var_dump($_GET);
-echo "Post: \n";
-var_dump($_POST);
-echo "Server: \n";
-var_dump($_SERVER);
+// echo "Get: \n";
+// var_dump($_GET);
+// echo "Post: \n";
+// var_dump($_POST);
+// echo "Server: \n";
+// var_dump($_SERVER);
--- tests/ValidazioneTest.php
+++ tests/ValidazioneTest.php
@@ -7,11 +7,160 @@ use App\Validation\ContactFormValidator;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
-class ResponseTest extends TestCase {
+class ValidazioneTest extends TestCase {
 
     public function testValidatorName() {
         $validator = new ContactFormValidator();
 
-        $this->assertEquals();
+        $risultatoValidate = $validator->validate([
+             'data'=>["name"=> " "]
+            ]
+        );
+        $this->assertEquals('Il nome è obbligatorio (min 2 caratteri).', $risultatoValidate['errors']['name']);
+
+
+        $risultatoValidate = $validator->validate([
+             'data'=>["name"=> "i"]
+            ]
+        );
+        $this->assertEquals('Il nome è obbligatorio (min 2 caratteri).', $risultatoValidate['errors']['name']);
+    }
+
+    public function testValidatorEmail() {
+
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+             'data'=>["email"=> " "]
+            ]
+        );
+        $this->assertEquals('Inserisci una email valida.', $risultatoValidate['errors']['email']);
+
+        $risultatoValidate = $validator->validate([
+             'data'=>["email"=> "mail"]
+            ]
+        );
+        $this->assertEquals('Inserisci una email valida.', $risultatoValidate['errors']['email']);
+    }
+
+    public function testValidatorMessage() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+             'data'=>["message"=> " "]
+            ]
+        );
+        $this->assertEquals('Il messaggio è obbligatorio (min 10 caratteri).', $risultatoValidate['errors']['message']);
+
+        $risultatoValidate = $validator->validate([
+             'data'=>["message"=> "esempio"]
+            ]
+        );
+        $this->assertEquals('Il messaggio è obbligatorio (min 10 caratteri).', $risultatoValidate['errors']['message']);
+    }
+
+    public function testOutput() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>"Irene",
+                'email'=>"irene@esempio.com",
+                'message'=>"Buonasera. Vorrei richiedere maggiori informazioni."
+            ]
+        );
+        $this->assertIsArray($risultatoValidate);
+
+        $this->assertArrayHasKey('name', $risultatoValidate['data']);
+        $this->assertArrayHasKey('email', $risultatoValidate['data']);
+        $this->assertArrayHasKey('message', $risultatoValidate['data']);
+
+        $this->assertArrayHasKey('errors', $risultatoValidate);
+        $this->assertArrayNotHasKey('name', $risultatoValidate['errors']);
+        $this->assertArrayNotHasKey('email', $risultatoValidate['errors']);
+        $this->assertArrayNotHasKey('message', $risultatoValidate['errors']);
+    }
+
+    public function testData() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>"I",
+                'email'=>"ireneesempio.com",
+                'message'=>"ciao"
+            ]
+        );
+        $this->assertArrayHasKey('name', $risultatoValidate['data']);
+        $this->assertArrayHasKey('email', $risultatoValidate['data']);
+        $this->assertArrayHasKey('message', $risultatoValidate['data']);
+    }
+
+    public function testErrors() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>"I",
+                'email'=>"ireneesempio.com",
+                'message'=>"ciao"
+            ]
+        );
+        $this->assertArrayHasKey('name', $risultatoValidate['errors']);
+        $this->assertArrayHasKey('email', $risultatoValidate['errors']);
+        $this->assertArrayHasKey('message', $risultatoValidate['errors']);
+    }
+
+    public function testRemoveHtml() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>"<b>Ire</b>",
+                'email'=>"<b>irene@esempio.com</b>",
+                'message'=>"<i>Buonasera.</i> Vorrei richiedere maggiori informazioni."
+            ]
+        );
+        $this->assertEquals('Ire', $risultatoValidate['data']['name']);
+        $this->assertEquals('irene@esempio.com', $risultatoValidate['data']['email']);
+        $this->assertEquals('Buonasera. Vorrei richiedere maggiori informazioni.', $risultatoValidate['data']['message']);
+    }
+
+    public function testSpaziMultipli() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>"Pinco     Pallo",
+                'message'=>"Buonasera.         Vorrei richiedere    maggiori informazioni."
+            ]
+        );
+        $this->assertEquals('Pinco Pallo', $risultatoValidate['data']['name']);
+        $this->assertEquals('Buonasera. Vorrei richiedere maggiori informazioni.', $risultatoValidate['data']['message']);
+    }
+
+    public function testTrim() {
+        $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>" Ire  ",
+                'email'=>"  irene@esempio.com ",
+                'message'=>"  Buonasera. Vorrei richiedere maggiori informazioni. "
+            ]
+        );
+        $this->assertEquals('Ire', $risultatoValidate['data']['name']);
+        $this->assertEquals('irene@esempio.com', $risultatoValidate['data']['email']);
+        $this->assertEquals('Buonasera. Vorrei richiedere maggiori informazioni.', $risultatoValidate['data']['message']);
+    }
+
+    public function testInputNonStringa() {
+         $validator = new ContactFormValidator();
+
+        $risultatoValidate = $validator->validate([
+                'name'=>['Irene'],
+                'email'=>['esempio@mail.com'],
+                'message'=>['Buonasera']
+            ]
+        );
+        $this->assertEquals('Il nome è obbligatorio (min 2 caratteri).', $risultatoValidate['errors']['name']);
+
+        $this->assertEquals('Inserisci una email valida.', $risultatoValidate['errors']['email']);
+
+        $this->assertEquals('Il messaggio è obbligatorio (min 10 caratteri).', $risultatoValidate['errors']['message']);
     }
 }
